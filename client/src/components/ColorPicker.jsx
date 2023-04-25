5876f392eca2e00766b4c5f1488535727a98df99
PetrovEvgeniy <evgeniypetrov945@gmail.com>
2023-04-25 15:58:01 +0200

    Add custom color picker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {SketchPicker} from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>ColorPicker</div>
+    <div
+    className='absolute left-full ml-3'>
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => state.color = color.hex}
+        // presetColors={[]} for adding custom colors
+      />
+    </div>
   )
 }
 
