05a996c78baff6b0e97773c41e5b993294ddb435
PetrovEvgeniy <evgeniypetrov945@gmail.com>
2023-04-30 01:16:23 +0200

    Add build folder

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://t-shirt-builder-api.onrender.com/api/v1/dalle",
   },
 };
 
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -58,7 +58,7 @@ const Customizer = () => {
             setGeneratingImg(true);
 
         
-            const response = await fetch('http://localhost:8080/api/v1/dalle', {
+            const response = await fetch('https://t-shirt-builder-api.onrender.com/api/v1/dalle', {
                method: 'POST',
                headers: {
                 'Content-Type': 'application/json',
